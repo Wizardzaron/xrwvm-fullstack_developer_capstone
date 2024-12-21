d424dc4db92c89fe064ff401bad0c5d1810f3981
Wizardzaron <89619552+Wizardzaron@users.noreply.github.com>
2024-12-21 15:52:09 -0800

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -44,8 +44,8 @@ class CarModel(models.Model):
 
     types = models.CharField(max_length=100)
     year = models.IntegerField(default=2023,
-                            validators=[
-                                MaxValueValidator(2023), 
+                               validators=[
+                                MaxValueValidator(2023),
                                 MinValueValidator(2015)
                             ])
 
