171c026bed7240c6610a68653d6e3459d349d593
Wizardzaron <89619552+Wizardzaron@users.noreply.github.com>
2024-12-21 19:06:29 -0800

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,7 +4,7 @@
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
+from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
 from .models import CarMake, CarModel
@@ -40,12 +40,14 @@ def login_user(request):
 
 # Create a `logout_request` view to handle sign out request
 
+
 def logout_request(request):
     logout(request)
     data = {"userName": ""}
     return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
+
 
 @csrf_exempt
 def registration(request):
@@ -57,30 +59,34 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except Exception as e:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(
+                                        username=username,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
+                                        email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
+    else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+
 
 def get_dealerships(request, state="All"):
     if (state == "All"):
@@ -116,29 +122,33 @@ def get_dealer_details(request, dealer_id):
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
+
 # Create a `add_review` view to submit a review
 def add_review(request):
-    
+
     if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
             post_review(data)
             return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+        except Exception as e:
+            return JsonResponse({
+                                "status": 401,
+                                "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
-    print (count)
-    if(count == 0):
+    print(count)
+    if (count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
         cars.append({
-                    "CarModel": car_model.name, "CarMake": car_model.car_make.name
+                    "CarModel": car_model.name,
+                    "CarMake": car_model.car_make.name
                     })
     return JsonResponse({"CarModels": cars})
