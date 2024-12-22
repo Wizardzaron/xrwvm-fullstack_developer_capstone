45c3fa76ee196e3860d619ea6df40bf44aeef4e1
Wizardzaron <89619552+Wizardzaron@users.noreply.github.com>
2024-12-21 19:18:14 -0800

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,8 +32,7 @@ ALLOWED_HOSTS = ['localhost',
                  'https://ryanhaddadi8-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
                 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://ryanhaddadi8-8000.
-        theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://ryanhaddadi8-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
