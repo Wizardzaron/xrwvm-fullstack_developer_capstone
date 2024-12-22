591ecbcb3bd5965ad54079e106230d154d5fea65
Wizardzaron <89619552+Wizardzaron@users.noreply.github.com>
2024-12-22 10:09:42 -0800

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-     (
+    (
          'https://ryanhaddadi8-8000.theiadockernext-1-labs-prod'
          '-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-     )
+    )
 ]
 CSRF_TRUSTED_ORIGINS = [
     (
